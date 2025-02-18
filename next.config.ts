5ff1283c09746f257ea639b25f9e240f42588b59
cuongpq1000vn <phamquoccuongv@gmail.com>
2025-02-18 12:20:20 +0700

    config to read token

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Bỏ qua lỗi ESLint khi build trên AWS
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Set-Cookie",
+            value: "SameSite=None; Secure",
+          },
+        ],
+      },
+    ];
+  },
   output: "standalone", // Đảm bảo Next.js build đúng cho Amplify
   experimental: {
     appDir: true, // Kích hoạt App Router (quan trọng)
