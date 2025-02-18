bb3b19df85f1b790c4faf75c2c243b63e3cce8fd
cuongpq1000vn <phamquoccuongv@gmail.com>
2025-02-18 12:11:43 +0700

    support ssr

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-module.exports = {
+const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Bỏ qua lỗi ESLint khi build trên AWS
+  },
+  output: "standalone", // Đảm bảo Next.js build đúng cho Amplify
+  experimental: {
+    appDir: true, // Kích hoạt App Router (quan trọng)
   },
 };
-module.exports = {
-  output: "standalone",
-};
+
+export default nextConfig;
