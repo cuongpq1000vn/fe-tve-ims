0d14941f66c45e85fcf0398342824e98d3701142
cuongpq1000vn <phamquoccuongv@gmail.com>
2025-02-18 12:02:11 +0700

    test

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -13,7 +13,7 @@ export default async function DashBoard() {
       <div className="min-h-screen p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
           <div className="bg-white shadow rounded-lg p-6">
-            <h2 className="text-lg font-medium text-gray-500">Students</h2>
+            <h2 className="text-lg font-medium text-gray-500">Students Test</h2>
             <p className="text-3xl font-bold text-gray-800 mt-2">
               {student.data}
             </p>
